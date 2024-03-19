68e43a514cd847638b01ffedfb42dfeace9cd7de
Rahul Prajapati <rprajapati@boruapps.com>
2024-03-19 17:20:09 +0530

    tab_v_1.1.6 voted barcode, 5

--- ios/Plugin/BarcodeScanner.swift
+++ ios/Plugin/BarcodeScanner.swift
@@ -16,6 +16,8 @@ typealias MLKitBarcodeScanner = MLKitBarcodeScanning.BarcodeScanner
     private var cameraView: BarcodeScannerView?
     private var scanCompletionHandler: (([Barcode]?, AVCaptureVideoOrientation?, String?) -> Void)?
 
+    private var barcodeRawValueVotes = [String: Int]()
+
     init(plugin: BarcodeScannerPlugin) {
         self.plugin = plugin
     }
@@ -50,6 +52,7 @@ typealias MLKitBarcodeScanner = MLKitBarcodeScanning.BarcodeScanner
             self.cameraView = nil
         }
         self.scanCompletionHandler = nil
+        self.barcodeRawValueVotes.removeAll()
     }
 
     @objc public func readBarcodesFromImage(imageUrl: URL, settings: ScanSettings, completion: @escaping ([Barcode]?, String?) -> Void) {
@@ -283,6 +286,18 @@ typealias MLKitBarcodeScanner = MLKitBarcodeScanning.BarcodeScanner
     private func handleScannedBarcode(barcode: Barcode, imageSize: CGSize, videoOrientation: AVCaptureVideoOrientation?) {
         plugin.notifyBarcodeScannedListener(barcode: barcode, imageSize: imageSize, videoOrientation: videoOrientation)
     }
+
+    private func voteForBarcode(barcode: Barcode) -> Int {
+        guard let rawValue = barcode.rawValue else {
+            return 1
+        }
+        if let votes = self.barcodeRawValueVotes[rawValue] {
+            self.barcodeRawValueVotes[rawValue] = votes + 1
+        } else {
+            self.barcodeRawValueVotes[rawValue] = 1
+        }
+        return self.barcodeRawValueVotes[rawValue] ?? 1
+    }
 }
 
 extension BarcodeScanner: BarcodeScannerViewDelegate {
@@ -292,7 +307,10 @@ extension BarcodeScanner: BarcodeScannerViewDelegate {
             self.stopScan()
         } else {
             for barcode in barcodes {
-                self.handleScannedBarcode(barcode: barcode, imageSize: imageSize, videoOrientation: videoOrientation)
+                let votes = self.voteForBarcode(barcode: barcode)
+                if votes >= 5 {
+                    self.handleScannedBarcode(barcode: barcode, imageSize: imageSize, videoOrientation: videoOrientation)
+                }
             }
         }
     }
