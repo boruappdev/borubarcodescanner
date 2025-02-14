ab5e1b6bdeb2f918e7f7a5ef249958a7f9b8267d
vbarochia <vbarochia@boruapps.com>
2025-02-14 14:49:28 +0530

    62853-vatescanlimit add scan delay

---
+++ android/src/main/java/io/capawesome/capacitorjs/plugins/mlkit/barcodescanning/classes/options/SetscanDelayMillisOptions.java
@@ -0,0 +1,13 @@
+package io.capawesome.capacitorjs.plugins.mlkit.barcodescanning.classes.options;
+
+public class SetscanDelayMillisOptions {
+    private int scanDelayMillis;
+
+  public SetscanDelayMillisOptions(int scanDelayMillis) {
+    this.scanDelayMillis = scanDelayMillis;
+  }
+
+    public float getscanDelayMillis() {
+        return scanDelayMillis;
+    }
+}
